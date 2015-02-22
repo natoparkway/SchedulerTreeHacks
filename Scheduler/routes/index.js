e7e62befed24498a12bbb980c9241661e346fc48
Matt Mistele <mmistele@stanford.edu>
2015-02-21 16:47:05 -0800

    Working on drag n drop for scheduling

--- Scheduler/routes/index.js
+++ Scheduler/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var test = [];
+
 /* GET home page. */
 router.get('/', function(req, res) {
 	req.session.major = "Computer Science";
@@ -11,8 +13,14 @@ router.get('/courses', function(req, res) {
 	res.render('sample', {title: 'Page2', major: req.session.major});
 });
 
+router.get('/schedule', function(req, res) {
+	res.render('scheduling', {title: 'Schedule'});
+});
+
 router.get('/data/major_info', function(req, res) {
 	res.json(req.session.major);
 });
+
+
 
 module.exports = router;
